1b58487d6d4ca8c8627e5de5e507ec1f2b93a950
Dylan <dylanlancekemboi@gmail.com>
2023-11-15 08:33:33 +0300

    Completed Assignment

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,16 +1,50 @@
 import React from "react";
 
-function Item({ item }) {
+// Destructure the onUpdateItem and onDeleteItem prop
+function Item({ item, onUpdateItem, onDeleteItem }) {
+  // Add function to handle button click
+  function handleAddToCartClick() {
+    // add fetch request
+    fetch(`http://localhost:4000/items/${item.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      // Since our goal is to let users add or remove items from their cart, we need to toggle the isInCart property of the item
+      body: JSON.stringify({
+        isInCart: !item.isInCart,
+      }),
+    })
+    .then((r) => r.json())
+    // Call onUpdateItem, passing the data returned from the fetch request
+    .then((updatedItem) => onUpdateItem(updatedItem));
+  }
+
+  // event listener function to enable delete
+  function handleDeleteClick() {
+    fetch(`http://localhost:4000/items/${item.id}`, {
+      method: "DELETE",
+    })
+    .then((r) => r.json())
+    .then(() => onDeleteItem(item));
+  }
+
   return (
     <li className={item.isInCart ? "in-cart" : ""}>
       <span>{item.name}</span>
       <span className="category">{item.category}</span>
-      <button className={item.isInCart ? "remove" : "add"}>
+      {/* add the onClick listener */}
+      <button 
+        className={item.isInCart ? "remove" : "add"}
+        onClick={handleAddToCartClick}
+      >
         {item.isInCart ? "Remove From" : "Add to"} Cart
       </button>
-      <button className="remove">Delete</button>
+      {/* Delete event listener */}
+      <button className="remove" onClick={handleDeleteClick}>Delete</button>
     </li>
   );
 }
 
 export default Item;
+
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
@@ -6,6 +6,36 @@ import Item from "./Item";
 function ShoppingList() {
   const [selectedCategory, setSelectedCategory] = useState("All");
   const [items, setItems] = useState([]);
+
+  // Add useEffect hook to fetch data by GET and store the data to the array items from state
+  useEffect(() => {
+    fetch("http://localhost:4000/items")
+    .then((r) => r.json())
+    .then((items) => setItems(items))
+  }, []);
+
+  // a callback f(x) passed as a prop so that Item component can send the updated item up to the ShoppingList component
+  function handleUpdateItem(updatedItem) {
+    const updatedItems = items.map((item) => {
+      if (item.id === updatedItem.id) {
+        return updatedItem;
+      } else {
+        return item;
+      }
+    });
+    setItems(updatedItems);
+  }
+
+  // add this callback f(x)
+  function handleDeleteItem(deletedItem) {
+    const updatedItems = items.filter((item) => item.id !== deletedItem.id);
+    setItems(updatedItems);
+  }
+
+  // a callback f(x) passed as a prop so that ItemForm component can send the new item up to the ShoppingList component
+  function handleAddItem(newItem) {
+    console.log([...items], newItem);
+  }
 
   function handleCategoryChange(category) {
     setSelectedCategory(category);
@@ -19,14 +49,16 @@ function ShoppingList() {
 
   return (
     <div className="ShoppingList">
-      <ItemForm />
+      {/* add the onAddItem prop! */}
+      <ItemForm onAddItem={handleAddItem}/>
       <Filter
         category={selectedCategory}
         onCategoryChange={handleCategoryChange}
       />
       <ul className="Items">
+        {/* pass it as a prop to Item */}
         {itemsToDisplay.map((item) => (
-          <Item key={item.id} item={item} />
+          <Item key={item.id} item={item} onUpdateItem={handleUpdateItem} onDeleteItem={handleDeleteItem}/>
         ))}
       </ul>
     </div>
